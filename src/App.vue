1f95e066da307f3dd3b0476d3bfd8f06465e7f90
akiho102517 <akiho102517@gmail.com>
2020-02-10 03:21:06 +0800

    跑馬燈至頂

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <div class="space_frame">
-      <div class="marquee_ctn fx aic">
-        <img class="laba" src="@img/laba.png" />
-        <marquee-text class="_fill">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.</marquee-text>
+      <div class="marquee_frame">
+        <div class="marquee_ctn fx aic">
+          <img class="laba" src="@img/laba.png" />
+          <marquee-text class="_fill">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna.</marquee-text>
+        </div>
       </div>
       <div class="marquee_ctn_fill"></div>
       <router-view />
