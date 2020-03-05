41b604594a11aa38e5d30b3eee7365302103a952
Reaven <y93209@gmail.com>
2020-03-06 01:31:17 +0800

    router go home latout

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
           <img class="login" src="@img/user.png" @click="login_show = true" />
         </div>
         <div class="h_ctn _nav fx aic ctn1">
-          <div class="index_logo" :style="{backgroundImage: `url(${siteInfo.LogoUrl})`}"></div>
+          <div class="index_logo" v-if="isHomePage" :style="{backgroundImage: `url(${siteInfo.LogoUrl})`}"></div>
+          <div class="index_logo page_go_back" v-else @click="$router.go(-1)"></div>
           <div class="kw_box _fill">
             <div class="kw_item fx aic">
               <input v-model="keyword" type="text" placeholder="輸入關鍵字查詢" class="_fill" />
