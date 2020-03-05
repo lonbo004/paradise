6631ce2992656e77b4fa64278145387f862939b3
akiho102517 <akiho102517@gmail.com>
2020-03-06 01:32:10 +0800

    GCard

--- src/components/GCard.vue
+++ src/components/GCard.vue
@@ -4,39 +4,39 @@
       <img :src="photo" />
     </div>
     <div class="_data">
-      <div class="_name">林雁兒</div>
+      <div class="_name">{{GInfo.name}}</div>
       <div class="_list clear">
         <div>
           國别:
-          <span>港澳妹</span>
+          <span>{{GInfo.country_name}}</span>
         </div>
         <div class="_50">
           年齡:
-          <span>20</span>
+          <span>{{GInfo.age}}</span>
         </div>
         <div class="_50">
           身高:
-          <span>164</span>
+          <span>{{GInfo.height}}</span>
         </div>
         <div class="_50">
           體重:
-          <span>暫無</span>
+          <span>{{GInfo.weight}}</span>
         </div>
         <div class="_50">
           罩杯:
-          <span>E</span>
+          <span>{{GInfo.cup}}</span>
         </div>
         <div>
           環境:
-          <span>外約</span>
+          <span>{{GInfo.environment_name}}</span>
         </div>
         <div>
           類型:
-          <span>外約</span>
+          <span>{{GInfo.service_type_name}}</span>
         </div>
-        <div>
+        <div v-if="price">
           托播價:
-          <span class="_price">8200</span>
+          <span class="_price">{{price}}</span>
         </div>
       </div>
     </div>
@@ -51,6 +51,17 @@ export default {
   computed: {
     photo() {
       return (this.GInfo.LadyFileList[0] || {}).path;
+    },
+    price(){
+      const sl = this.GInfo.shorttime_local_final_price;
+      const so = this.GInfo.shorttime_outside_final_price;
+      const ll = this.GInfo.longtime_local_final_price;
+      const lo = this.GInfo.longtime_outside_final_price;
+      const _price = [sl , so , ll , lo];
+      if(_price.every(x=>x===0)) return false;
+      const _max = Math.max(..._price);
+      const _min = Math.min(..._price);
+      return [_min, _max].filter(x=>x>0).join("~");
     }
   }
 };
