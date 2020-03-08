b0a271e0295319b91ffcd2b6aab4ce7dd3d51570
akiho102517 <akiho102517@gmail.com>
2020-03-08 19:29:48 +0800

    進階

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,15 @@
 const getters = {
   siteInfo: state => state.siteData.Site || {},
+  //siteData
   MarqueeList: state => state.siteData.MarqueeList || [],
-  TownList: state => (state.siteData.Site || {}).TownList || []
+  EnvironmentList: state => state.siteData.EnvironmentList || [],
+  CupList: state => state.siteData.CupList || [],
+  ServiceModeList: state => state.siteData.ServiceModeList || [],
+  AllowServiceList: state => state.siteData.AllowServiceList || [],
+  //siteInfo
+  TownList: state => (state.siteData.Site || {}).TownList || [],
+  CountryList: state => (state.siteData.Site || {}).CountryList || [],
+  ServiceTypeList: state => (state.siteData.Site || {}).ServiceTypeList || [],
+
 }
 export default getters
--- src/views/search.vue
+++ src/views/search.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="search_frame ctn0">
+    <div class="search_ctn" v-if="me_list.length">
+      <div class="search_title">使用進階組合搜尋（*不填則搜尋所有）：</div>
+      <div class="fx fw jcsb me_fill">
+        <MeCard :class="'_kw'" v-for="(item,index) in me_list" :meInfo="item" />
+      </div>
+      <div>
+        <pagination :total="count" :page.sync="params.page" :limit.sync="params.page_range" @pagination="getData" />
+      </div>
+    </div>
     <div class="search_ctn">
       <div class="search_title">使用進階組合搜尋（*不填則搜尋所有）：</div>
       <div class="search_form">
-        <el-form ref="form" :model="params">
+        <el-form>
           <el-row>
             <el-col :sm="8">
               <el-form-item class="form_label" label="菜品暱稱">
@@ -12,25 +21,21 @@
             </el-col>
             <el-col :sm="8">
               <el-form-item class="form_label" label="服務縣市">
-                <el-select class="type_input" v-model="params.city" placeholder="請選擇市縣">
-                  <el-option :label="item.Name" :value="item.Code" v-for="item in cityList">{{item.Name}}</el-option>
+                <el-select class="type_input" v-model="params.town_code" placeholder="請選擇市縣">
+                  <el-option :label="item.Name" :value="item.Code" v-for="item in TownList" @click.native="sd_TownList = item"></el-option>
                 </el-select>
               </el-form-item>
             </el-col>
             <el-col :sm="8">
               <el-form-item class="form_label" label="服務地區">
-                <el-select class="type_input search_from_select" v-model="params.area" multiple placeholder="請選擇服務地區" popper-class="search_from_select" @change="areaSelect">
+                <el-select class="type_input search_from_select" multiple placeholder="請選擇服務地區" popper-class="search_from_select">
                   <div class="fx sp_option">
-                    <el-option :value="'all'">
-                      <span>全選</span>
-                    </el-option>
-                    <el-option :value="'clear'">
-                      <span>清除</span>
-                    </el-option>
+                    <span @click="sp_district_code('all')">全選</span>
+                    <span @click="sp_district_code('clear')">清除</span>
                   </div>
-                  <el-option :label="item.Name" :value="item.Code" v-for="item in areaList">
-                    <el-checkbox-group class="check_group" v-model="params.area">
-                      <el-checkbox :label="item.Name" :value="item.Code">{{item.Name}}</el-checkbox>
+                  <el-option :label="item.Name" :value="item.Code" v-for="item in DistrictList">
+                    <el-checkbox-group class="check_group" v-model="params.district_code">
+                      <el-checkbox :label="item.Code">{{item.Name}}</el-checkbox>
                     </el-checkbox-group>
                   </el-option>
                 </el-select>
@@ -38,69 +43,69 @@
             </el-col>
             <el-col :sm="8">
               <el-form-item class="form_label" label="年龄">
-                <el-input class="type_input" v-model="params.name" placeholder="如:20,或:18-25"></el-input>
+                <el-input class="type_input" v-model="params.age" placeholder="如:20,或:18-25"></el-input>
               </el-form-item>
             </el-col>
             <el-col :sm="8">
               <el-form-item class="form_label" label="身高">
-                <el-input class="type_input" v-model="params.name" placeholder="如:168,或:160-170"></el-input>
+                <el-input class="type_input" v-model="params.height" placeholder="如:168,或:160-170"></el-input>
               </el-form-item>
             </el-col>
             <el-col :sm="8">
               <el-form-item class="form_label" label="體重">
-                <el-input class="type_input" v-model="params.name" placeholder="如:56,或:56-60"></el-input>
+                <el-input class="type_input" v-model="params.weight" placeholder="如:56,或:56-60"></el-input>
               </el-form-item>
             </el-col>
             <el-col :sm="8">
               <el-form-item class="form_label" label="托播價">
-                <el-input class="type_input" v-model="params.name" placeholder="如:1000,或:1000-3500"></el-input>
+                <el-input class="type_input" v-model="params.price" placeholder="如:1000,或:1000-3500"></el-input>
               </el-form-item>
             </el-col>
             <el-col :sm="24">
               <el-form-item class="form_label" label="國籍">
-                <el-checkbox-group v-model="params.countryList" class="check_group">
-                  <el-checkbox :label="item.Code" v-for="item in countryList" :value="item.Code">{{item.Name}}</el-checkbox>
+                <el-checkbox-group v-model="params.country_code" class="check_group">
+                  <el-checkbox :label="item.Code" v-for="item in CountryList" :value="item.Code">{{item.Name}}</el-checkbox>
                 </el-checkbox-group>
               </el-form-item>
             </el-col>
             <el-col :sm="24">
               <el-form-item class="form_label" label="服務環境">
-                <el-checkbox-group v-model="params.environmentList" class="check_group">
-                  <el-checkbox :label="item.code" v-for="item in environmentList" :value="item.code">{{item.name}}</el-checkbox>
+                <el-checkbox-group v-model="params.environment_code" class="check_group">
+                  <el-checkbox :label="item.code" v-for="item in EnvironmentList" :value="item.code">{{item.name}}</el-checkbox>
                 </el-checkbox-group>
               </el-form-item>
             </el-col>
             <el-col :sm="24">
               <el-form-item class="form_label" label="罩杯">
-                <el-checkbox-group v-model="params.cupList" class="check_group">
-                  <el-checkbox :label="item.code" v-for="item in cupList" :value="item.code">{{item.name}}</el-checkbox>
+                <el-checkbox-group v-model="params.cup_code" class="check_group">
+                  <el-checkbox :label="item.code" v-for="item in CupList" :value="item.code">{{item.name}}</el-checkbox>
                 </el-checkbox-group>
               </el-form-item>
             </el-col>
             <el-col :sm="8">
               <el-form-item class="form_label" label="服務方式">
-                <el-checkbox-group v-model="params.serviceTypeList" class="check_group">
-                  <el-checkbox :label="item.Code" v-for="item in serviceTypeList" :value="item.Code">{{item.Name}}</el-checkbox>
+                <el-checkbox-group v-model="params.service_type_code" class="check_group">
+                  <el-checkbox :label="item.Code" v-for="item in ServiceTypeList" :value="item.Code">{{item.Name}}</el-checkbox>
                 </el-checkbox-group>
               </el-form-item>
             </el-col>
             <el-col :sm="16">
               <el-form-item class="form_label" label="服務模式">
-                <el-checkbox-group v-model="params.serviceModeList" class="check_group">
-                  <el-checkbox :label="item.code" v-for="item in serviceModeList" :value="item.code">{{item.name}}</el-checkbox>
+                <el-checkbox-group v-model="params.service_mode_code" class="check_group">
+                  <el-checkbox :label="item.code" v-for="item in ServiceModeList" :value="item.code">{{item.name}}</el-checkbox>
                 </el-checkbox-group>
               </el-form-item>
             </el-col>
             <el-col :sm="24">
               <el-form-item class="form_label" label="可配合">
-                <el-checkbox-group v-model="params.allowServiceList" class="check_group">
-                  <el-checkbox :label="item.code" v-for="item in allowServiceList" :value="item.code">{{item.name}}</el-checkbox>
+                <el-checkbox-group v-model="params.allow_service" class="check_group">
+                  <el-checkbox :label="item.code" v-for="item in AllowServiceList" :value="item.code">{{item.name}}</el-checkbox>
                 </el-checkbox-group>
               </el-form-item>
             </el-col>
             <el-col :sm="24">
               <el-form-item>
-                <el-button @click="searchMeiMei" class="submit">送出</el-button>
+                <el-button @click="getData" class="submit">送出</el-button>
               </el-form-item>
             </el-col>
           </el-row>
@@ -112,81 +117,65 @@
 
 <script>
 import { mapState, mapGetters } from "vuex";
-
+//api
+import { Lady_Search } from "@/api";
+//components
+import MeCard from "@c/MeCard";
+import pagination from "@c/pagination";
 export default {
+  components: { MeCard, pagination },
   data() {
     return {
+      me_list: [],
+      count: 0,
       params: {
-        area: [],
-        name: '',
-        city: '',
-        cupList: [],
-        countryList: [],
-        allowServiceList: [],
-        serviceTypeList: [],
-        serviceModeList: [],
-        environmentList: []
+        name: "",
+        town_code: "",
+        district_code: [],
+        age: "",
+        height: "",
+        weight: "",
+        country_code: [],
+        price: "",
+        environment_code: [],
+        service_type_code: [],
+        service_mode_code: [],
+        cup_code: [],
+        allow_service: [],
+        page: 1,
+        page_range: 10,
       },
-      oldChooseData: [],
+      sd_TownList: undefined,
+      DistrictList: [],
     }
   },
   computed: {
-    ...mapState(["siteData"]),////test siteData
-    ...mapGetters(["siteInfo", "TownList"]),
-    environmentList() {
-      return this.siteData.EnvironmentList
-    },
-    serviceModeList() {
-      return this.siteData.ServiceModeList
-    },
-    allowServiceList() {
-      return this.siteData.AllowServiceList
-    },
-    cupList() {
-      return this.siteData.CupList
-    },
-    countryList() {
-      return this.siteInfo.CountryList
-    },
-    serviceTypeList() {
-      return this.siteInfo.ServiceTypeList
-    },
-    cityList() {
-      return this.TownList
-    },
-    areaList() {
-      let res = []
-      let city = !!this.params.city
-      if (city) {
-        res = this.TownList.filter((tw) => tw.Code === this.params.city)[0].DistrictList
-      }
-      return res
-    },
+    ...mapGetters(["siteInfo", "TownList", "CountryList", "EnvironmentList", "CupList", "ServiceTypeList", "ServiceModeList", "AllowServiceList"]),
   },
   methods: {
-    searchMeiMei() {
-      console.log('submit!');
+    getData() {
+      Lady_Search(params).then(res => {
+        this.page = 1;
+        this.me_list = res.LadyList;
+        this.count = res.count;
+      })
     },
-    areaSelect(val) {
-      const allValues = this.areaList.map(item => {
-        return item.Code;
-      });
-      const oldVal = this.oldChooseData.length > 0 ? this.oldChooseData : []
-      if (val.includes('all')) {
-        this.params.area = allValues;
+    sp_district_code(type) {
+      if (type === "all") {
+        this.params.district_code = this.sd_TownList.DistrictList.map(x => x.Code);
       }
-
-      // const res = item.find((obj, idx) => obj[idx] === 'all' || obj === 'clear')
-      // console.log(res)
-      // if (item[0] === 'all' || !res) {
-      //   console.log(item)
-      //   this.params.area = this.areaList.map(areas => areas.Code)
-      // }
-      // if (item[0] === 'clear' || res) {
-      //   console.log(item, res)
-      //   this.params.area = []
-      // }
+      else if (type === "clear") {
+        this.params.district_code.splice(0);
+      }
     }
   },
+  watch: {
+    sd_TownList: {
+      handler(val) {
+        this.DistrictList = val.DistrictList;
+        this.params.environment_code = [];
+      },
+    }
+  }
 };
 </script>
